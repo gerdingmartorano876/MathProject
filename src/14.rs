34755a460dbcf36120b02bbe48782058031c2256
gerdingmartorano876 <gerdingmartorano876@gmail.com>
2025-03-20 23:08:25 +0000

    Add 14.rs

---
+++ src/14.rs
@@ -0,0 +1,9 @@
+// This is just an example of how you might write Rust code.
+fn main() {
+    // Example code that calculates the sum of two numbers
+    let num1 = 5;
+    let num2 = 7;
+    let result = num1 + num2;
+
+    println!("The sum of {} and {} is {}", num1, num2, result);
+}
