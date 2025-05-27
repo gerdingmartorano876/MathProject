ead863e834a7190c622ed1beaef85272ee6992ef
gerdingmartorano876 <gerdingmartorano876@gmail.com>
2025-05-27 16:23:01 +0000

    Add 42.rs

---
+++ src/42.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Example Rust code to perform basic arithmetic operations
+    let num1 = 5;
+    let num2 = 3;
+    let result = num1 + num2;
+    println!("The sum of {} and {} is {}", num1, num2, result);
+}
